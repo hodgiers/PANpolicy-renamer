f603a0fd8baf339cf269707571d23b96e0ba64b1
Rashan Hodge <rhodge@macminiPro.local>
2023-07-24 14:55:05 -0400

    updated code

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Ingests PAN config via api call and renames the policies according to requiremen
 # Create a .env file in the directory this repo was cloned into.
 The .env file should contain:
 	```
-	PAN_API_KEY=<YOUR PAN API KEY>
-	DEVICE_GROUP=<THE DG THAT THE POLCIES NEED TO BE RANAMED ON>
-	FW_HOST=<HOSTNAME OR IP ADDRESS OF PANORAMA>
+	'PAN_API_KEY=<YOUR PAN API KEY>'
+	'DEVICE_GROUP=<THE DG THAT THE POLCIES NEED TO BE RANAMED ON>'
+	'FW_HOST=<HOSTNAME OR IP ADDRESS OF PANORAMA>'
 	```
 
 ## Build the dockerfile by running: 
--- grabandparse.py
+++ grabandparse.py
@@ -22,42 +22,59 @@ print(f"Hostname: {fw_host}")
 # REST API URL
 url_prerule = f"https://{fw_host}/restapi/v10.2/Policies/SecurityPreRules?location=device-group&device-group={device_group}"
 url_postrule = f"https://{fw_host}/restapi/v10.2/Policies/SecurityPostRules?location=device-group&device-group={device_group}"
+url_device_groups = f"https://{fw_host}/restapi/v10.2/Panorama/DeviceGroups"
+
 
 # Headers
 headers = {
     'X-PAN-KEY': api_key,
 }
 
+# Send the API request for device groups
+response_device_groups = requests.get(url_device_groups, headers=headers, verify=False)
+
+# Parse the JSON response
+data_device_groups = response_device_groups.json()
+
+# Write the device groups data to a file
+with open('device_groups.json', 'w') as f:
+    json.dump(data_device_groups, f)
+
 
 # Function to parse and print policy details
 def parse_and_print_policies(data):
     validation_required = []
     
-    for policy in data['result']['entry']:
-        tags = policy.get('tag', {}).get('member', [])
+    if 'entry' in data.get('result', {}):
+        for policy in data['result']['entry']:
+            tags = policy.get('tag', {}).get('member', [])
         
-        # Skip policies with the "Manually Added" tag
-        if 'Manually Added' in tags:
-            continue
+            # Skip policies with the "Manually Added" tag
+            if 'Manually Added' in tags:
+                continue
 
-        name = policy['@name']
-        description = policy.get('description', 'No description')
-        
-        # If the description contains "advanced" and "ifc", add to the validation list
-        if "advanced" in description and "ifc" in description:
-            validation_required.append(name)
-            continue
+            name = policy['@name']
+            description = policy.get('description', 'No description')
+            
+            # If there's no description, use the current name as the description
+            if description == 'No description':
+                description = name
+            
+            # If the description contains "advanced" and "ifc", add to the validation list
+            if "advanced" in description and "ifc" in description:
+                validation_required.append(name)
+                continue
 
-        # Find the text after "L4 RULE: " or "L7 RULE: "
-        match = re.search(r'(L4 RULE: |L7 RULE: )(.*)', description)
-        if match:
-            description = match.group(2)
-        
-        print(f'rename device-group {device_group} pre-rulebase security rules {name} to "{description}"\n')
+            # Find the text after "L4 RULE: " or "L7 RULE: "
+            match = re.search(r'(L4 RULE: |L7 RULE: )(.*)', description)
+            if match:
+                description = match.group(2)
+            
+            print(f'rename device-group {device_group} pre-rulebase security rules {name} to "{description}"\n')
 
-    print("\nThe following policies require validation as the description indicates issues with the migration:")
-    for policy in validation_required:
-        print(policy)
+        print("\nThe following policies require validation as the description indicates issues with the migration:")
+        for policy in validation_required:
+            print(policy)
 
 
 
@@ -69,7 +86,10 @@ data_pre = response_pre.json()
 
 # Print the pre-rulebase policy details
 print("Pre-Rulebase Security Policies:\n")
-parse_and_print_policies(data_pre)
+if 'entry' in data_pre.get('result', {}):
+    parse_and_print_policies(data_pre)
+else:
+    print('No entry found in the data for pre-rulebase.')
 
 # Send the API request for post-rulebase
 response_post = requests.get(url_postrule, headers=headers, verify=False)
@@ -79,4 +99,26 @@ data_post = response_post.json()
 
 # Print the post-rulebase policy details
 print("Post-Rulebase Security Policies:\n")
-parse_and_print_policies(data_post)
+if 'entry' in data_post.get('result', {}):
+    parse_and_print_policies(data_post)
+else:
+    print('No entry found in the data for post-rulebase.')
+
+
+# REST API URL for device groups
+url_device_groups = f"https://{fw_host}/restapi/v10.2/Panorama/DeviceGroups"
+
+# Send the API request for device groups
+response_device_groups = requests.get(url_device_groups, headers=headers, verify=False)
+
+# Parse the JSON response
+data_device_groups = response_device_groups.json()
+
+# Extract the names of all device groups
+device_group_names = [entry["@name"] for entry in data_device_groups.get("result", {}).get("entry", [])]
+
+# Save the device group names to a file
+with open("device_groups.txt", "w") as file:
+    for name in device_group_names:
+        file.write(name + "\n")
+
